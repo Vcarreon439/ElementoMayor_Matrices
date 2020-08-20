cfb66c245ce7b637571269f2843287f9e3cca42e
Victor <victor.carreon@pm.me>
2020-08-19 21:00:27 -0500

    Agregar archivos de proyecto.

---
+++ ElementoMayor_Matrices/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ElementoMayor_Matrices{
+    class Program{
+        static void Main(string[] args){
+
+            //Instancia de clase matrices
+            Matrices Objn = new Matrices();
+            //Imprime la matriz
+            Objn.ImprimeMatriz();
+            Console.ReadKey();
+        }
+    }
+}
